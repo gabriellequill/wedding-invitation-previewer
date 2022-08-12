c9dd2860e8de348b9a46ca49ba22b5cd6dcfd425
Gabrielle Quill <gabriellequill@MacBook-Air.modem>
2022-08-12 20:46:21 +1000

    Completed assignment

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-function version1 (){
-    document.getElementById ('concepts').src="Wedding_invite_plain.jpg";
-    document.getElementById ('responses1').style.fontFamily = 'mySecondFont';
+const img = document.getElementById('image')
+console.log(img)
+const btn1 = document.getElementById('minimalist')
+const btn2 = document.getElementById('rose')
+const btn3 = document.getElementById('vintage')
+btn1.addEventListener('click', showMinimalist)
+btn2.addEventListener('click', showRose)
+btn3.addEventListener('click', showVintage)
+
+function showMinimalist() {
+    img.className = 'minimalist'   
 }
+ function showRose() {
+    img.className = 'rose'
+ }
+ function showVintage() {
+    img.className = 'vintage'
+ }
 
-function version2 (){
-    document.getElementById ('concepts').src="Wedding_invite_rose.jpg";
-    document.getElementById ('responses2').style.fontFamily = 'myThirdFont';
+const showDetails = document.getElementById('details');
+const showDate = document.getElementById('date');
+const showTime = document.getElementById('time');
+const showLocation = document.getElementById('location');
+const showDetails1 = document.getElementById('details1');
+const showDate1 = document.getElementById('date1');
+const showTime1 = document.getElementById('time1');
+const showLocation1 = document.getElementById('location1');
+
+showDetails.addEventListener ('keyup', displayDetails);
+showDate.addEventListener ('keyup', displayDate);
+showTime.addEventListener ('keyup', displayTime);
+showLocation.addEventListener ('keyup', displayLocation);
+
+function displayDetails (e) {
+    showDetails1.innerText = e.target.value
 }
-
-function version3 (){
-    document.getElementById ('concepts').src="Wedding_invite_art_deco.jpg";
-    document.getElementById ('responses3').style.fontFamily = 'myFourthFont';
+function displayDate (e) {
+    showDate1.innerText = e.target.value
 }
-
-const showDetails = document.getElementsByClassName ('details');
-const showDate = document.getElementsByClassName ('date');
-const showTime = document.getElementsByClassName ('time');
-const showLocation = document.getElementsByClassName ('location');
-
-inputTarget.addEventListener ('keyup', displayInputValue);
-
-function displayInputValue () {
-    console.log (inputTarget.value)
-    showDetails[0].innerHTML = inputTarget.value
-    showDate[1].innerHTML = inputTarget.value
-    showTime[2].innerHTML=inputTarget.value
-    showLocation[3].innerHTML = inputTarget.value
+function displayTime (e) {
+    showTime1.innerText = e.target.value
+}
+function displayLocation (e) {
+    showLocation1.innerText = e.target.value
 }
 
